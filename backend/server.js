444891916e349efb0c90ec84e679abb500cc539e
Lakan Santos <rizesuoracle25@gmail.com>
2023-03-11 19:07:16 +0800

    added elements on frontend

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@ const express = require('express')
 
 const app = express()
 
+
 require('dotenv').config();
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
-})
 
 app.get('/', (req, res) => {
 
     res.json({mssg: 'yehey'})
     console.log('Yehey you ran it!!')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
 })
