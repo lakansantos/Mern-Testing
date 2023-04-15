a7c23674c72aefa7a183ef7ff5af62897ca76205
Lakan Santos <rizesuoracle25@gmail.com>
2023-04-15 23:28:36 +0800

    restructure

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const router = require('./routes/testroutes');
-app.use(cors())
-
 
 require('dotenv').config();
 
 const PORT = process.env.PORT
 
-app.use('/api/v1', router);
-
 app.use(cors({
     origin: 'http://localhost:4000'
 }))
+
+app.use('/api/v1', router);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
