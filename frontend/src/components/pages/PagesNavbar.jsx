282f85002e4cd0dbc92bc40899ad5bdea3b87473
Lakan Santos <rizesuoracle25@gmail.com>
2023-06-11 17:22:23 +0800

    added logout

--- frontend/src/components/pages/PagesNavbar.jsx
+++ frontend/src/components/pages/PagesNavbar.jsx
@@ -1,12 +1,18 @@
-const PagesNavbar = () => {
-    return (
-        <nav className="bg-primary">
-        <ul>
-            <li><a href="/views/home">Home</a></li>
-        </ul>
-    </nav>
-    )
-}
+import Logout from "../../views/logout/logout";
 
+const PagesNavbar = () => {
+  return (
+    <nav className="bg-primary">
+      <ul>
+        <li>
+          <a href="/views/home">Home</a>
+        </li>
+        <li>
+          <Logout />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default PagesNavbar;
---
+++ frontend/src/views/logout/logout.jsx
@@ -0,0 +1,17 @@
+import cookies from "../../utils/cookies";
+import {AUTH_COOKIE_NAME} from "../../configs/constants";
+import {Button} from "reactstrap";
+const Logout = () => {
+  const handleLogout = () => {
+    cookies.remove(AUTH_COOKIE_NAME, {path: "/"});
+    window.location.reload();
+  };
+
+  return (
+    <Button onClick={handleLogout} color="primary">
+      Logout
+    </Button>
+  );
+};
+
+export default Logout;
