fe70b2a871297fd28a2314c550196bb9cd83cc5c
Lakan Santos <rizesuoracle25@gmail.com>
2023-06-11 15:42:24 +0800

    added login middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,8 +4,9 @@ const { JWT_SECRET } = require("../config/environment");
 const verifyToken = async (req, res, next) => {
   if (req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
     try {
-      await jwt.verify(token, JWT_SECRET);
+      jwt.verify(token, JWT_SECRET);
       next();
     } catch (error) {
       res.status(400);
